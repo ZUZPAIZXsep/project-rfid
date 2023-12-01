23e5515b9d983b96d12ed72d882e34dd4a95aa78
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2023-12-01 15:22:53 +0700

    ok add save and reset
    ok add save and reset button

--- server/index.js
+++ server/index.js
@@ -1,33 +1,14 @@
-// index.js
 const express = require('express');
-const EventSource = require('eventsource');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+const http = require('http');
+const WebSocket = require('ws');
 const rfidReader = require('./rfidReader');
+
+const app = express();
 
 // Serve static files from the React build directory
 app.use(express.static(path.join(__dirname, 'local/build')));
-
-// SSE endpoint
-app.get('/sse', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  const sendRfidTags = () => {
-    const rfidTags = rfidReader.getRfidTags();
-    res.write(`data: ${JSON.stringify(rfidTags)}\n\n`);
-  };
-
-  sendRfidTags();
-
-  const intervalId = setInterval(sendRfidTags, 1000);
-
-  req.on('close', () => {
-    clearInterval(intervalId);
-  });
-});
 
 // Proxy requests to React app
 app.use(
@@ -43,7 +24,43 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'local/public/index.html'));
 });
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// WebSocket endpoint
+const ws = new WebSocket('ws://localhost:3000');
+
+wss.on('connection', (ws) => {
+  ws.send(JSON.stringify(rfidReader.getRfidTags()));
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+  // ส่งข้อมูลเมื่อมีการบันทึกหรือรีเซ็ต
+  ws.on('message', (message) => {
+    if (message === 'save') {
+      console.log('Saved Count:', rfidReader.getRfidTags().length);
+      ws.send(JSON.stringify(rfidReader.getRfidTags()));
+    } else if (message === 'reset') {
+      console.log('Reset Count');
+      rfidReader.resetRfidTags();
+      ws.send(JSON.stringify(rfidReader.getRfidTags()));
+    }
+  });
+});
+
+
+// Periodically send RFID tags to connected WebSocket clients
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(rfidReader.getRfidTags()));
+    }
+  });
+}, 1000);
+
 const port = 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
